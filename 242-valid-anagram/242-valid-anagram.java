4d00a01b8127a17133759d295aa7e55ab0f14d60
Kavya-0124 <81321487+Kavya-0124@users.noreply.github.com>
2022-09-27 17:39:39 +0530

    Time: 13 ms (37.05%), Space: 44.9 MB (37.65%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+     
+        char[] s_array=s.toCharArray();
+        char[] t_array=t.toCharArray();
+        
+        Arrays.sort(s_array);
+        Arrays.sort(t_array);
+        
+        if(Arrays.equals(s_array,t_array)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
