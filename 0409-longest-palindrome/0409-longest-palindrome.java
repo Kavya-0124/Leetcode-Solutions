97879e2de12b28581457d530e8b81bbd93311341
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2024-07-09 00:55:54 +0530

    Time: 5 ms (70.05%), Space: 42.4 MB (5.36%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,21 +1,41 @@
+// class Solution {
+//     public int longestPalindrome(String s) {
+//         HashMap<Character,Integer> charFrequency=new HashMap<>();
+//         int oddFreq=0;
+//         for(char ch:s.toCharArray()){
+//             charFrequency.put(ch,charFrequency.getOrDefault(ch,0)+1);
+//             if((charFrequency.get(ch)%2)==1){
+//                 oddFreq++;
+//             }
+//             else{
+//                 oddFreq--;
+//             }
+//         }
+//         if(oddFreq>0){
+//             return s.length()-oddFreq+1;
+//         }
+//         else{
+//             return s.length();
+//         }
+//     }
+// }
+
 class Solution {
     public int longestPalindrome(String s) {
-        HashMap<Character,Integer> charFrequency=new HashMap<>();
-        int oddFreq=0;
-        for(char ch:s.toCharArray()){
-            charFrequency.put(ch,charFrequency.getOrDefault(ch,0)+1);
-            if((charFrequency.get(ch)%2)==1){
-                oddFreq++;
+        Set<Character> chset=new HashSet<>();
+        int res=0;
+
+        for(char c:s.toCharArray()){
+            if(chset.contains(c)){
+                chset.remove(c);
+                res+=2;
             }
             else{
-                oddFreq--;
+                chset.add(c);
             }
         }
-        if(oddFreq>0){
-            return s.length()-oddFreq+1;
-        }
-        else{
-            return s.length();
-        }
+        if(!chset.isEmpty()) res++;
+
+        return res;
     }
 }
