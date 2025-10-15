74cda120f0e6ac9a4560f0aeb9f60c067e57ca2a
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2025-10-15 20:59:37 +0530

    Time: 2 ms (88.24%), Space: 44.9 MB (34.87%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -1,19 +1,16 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        int n=words.length;
-        List<Map<Character, Integer>> freq=new ArrayList<>();
-        for(String w:words){
-            Map<Character,Integer> map = new HashMap<>();
-            for(char c:w.toCharArray())
-                map.put(c,map.getOrDefault(c,0)+1);
-            freq.add(map);
+        String prev="";
+        List<String> li=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            char[] ch=words[i].toCharArray();
+            Arrays.sort(ch);
+            String curr=String.valueOf(ch);
+            if(!curr.equals(prev)){
+                li.add(words[i]);
+                prev=curr;
+            }
         }
-        List<String> ans=new ArrayList<>();
-        ans.add(words[0]);
-        for(int i=1;i<n;i++){
-            if(!freq.get(i).equals(freq.get(i-1)))
-            ans.add(words[i]);
-        }
-        return ans;
+        return li;
     }
 }
