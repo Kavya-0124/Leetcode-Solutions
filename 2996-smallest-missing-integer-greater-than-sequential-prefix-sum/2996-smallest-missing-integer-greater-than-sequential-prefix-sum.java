b94292e773f7fb2e7eb8fafacde6580d2214d8f0
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2026-02-22 16:45:25 +0530

    Time: 2 ms (55.32%), Space: 44.5 MB (12.61%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,21 +1,38 @@
 class Solution {
     public int missingInteger(int[] nums) {
         // Arrays.sort(nums);
-        int maxSum=nums[0];
-        int size=nums.length;
+        int maxSum = nums[0];
+        int size = nums.length;
+        // Set<Integer> map=new HashSet<>();
         // Arrays.sort(nums);
-        for(int i=1;i<size;i++){
-            if(nums[i]==nums[i-1]+1){
-                maxSum+=nums[i];
-            }
-            else break;
+        for (int i = 1; i < size; i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                maxSum += nums[i];
+            // map.put(maxSum,nums[i]);
+            // maxSum+=nums[i];
+            else
+                break;
         }
-        Arrays.sort(nums);
-        for(int i=0;i<size;i++){
-            if(maxSum==nums[i]){
-                maxSum++;
-            }
+        Set<Integer> set = toSet(nums);
+        while (true) {
+            if (!set.contains(maxSum))
+                return maxSum;
+            maxSum++;
         }
-        return maxSum;
     }
+
+    private Set<Integer> toSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums)
+            set.add(i);
+        return set;
+    }
+    // Arrays.sort(nums);
+    // for(int i=0;i<size;i++){
+    //     if(maxSum==nums[i]){
+    //         maxSum++;
+    //     }
+    // }
+    // return maxSum;
+    // }
 }
