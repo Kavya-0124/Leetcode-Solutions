de3cb179f1e9ab6e0d340bd850be7c4a4f6220de
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2026-03-07 21:42:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -3,7 +3,7 @@ class Solution {
         if(prices==null || prices.length<1) return 0;
         int buy1=-prices[0], sell1=0, buy2=-prices[0], sell2=0;
         for(int i=1;i<prices.length;i++){
-            buy1=Math.max(buy1,-prices[i]);
+            buy1=Math.max(buy1,prices[i]);
             sell1=Math.max(sell1,buy1+prices[i]);
             buy2=Math.max(buy2,sell1-prices[i]);
             sell2=Math.max(sell2, buy2+prices[i]);
