92bbd5074c24b588417c3b6f4f6e9b555f37358b
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2026-03-25 22:02:12 +0530

    Time: 5 ms (47.55%), Space: 161.7 MB (33.57%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        long total=0;
+        long sum = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                total+=grid[i][j];
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < m; j++) {
+                sum += grid[i][j];
+            }
+            if (sum == total-sum) {
+                return true;
+            }
+        }
+        sum=0;
+        for(int j=0;j<m-1;j++){
+            for(int i=0;i<n;i++){
+                sum+=grid[i][j];
+                if(sum==total-sum){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
