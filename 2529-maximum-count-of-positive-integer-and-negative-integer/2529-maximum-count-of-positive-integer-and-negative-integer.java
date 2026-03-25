5cfd4ef36a5671894770a12fda79b6f682ba02f0
Kavya Srivastava <81321487+Kavya-0124@users.noreply.github.com>
2026-03-25 23:29:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -5,7 +5,7 @@ class Solution {
             if(nums[i]>0){
                 pos++;
             }
-            if(nums[i]<0){
+            else if(nums[i]<0){
                 neg++;
             }
         }
